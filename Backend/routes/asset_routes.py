67cc835f1033916a408ead80717dca3a91fff465
Anbarasan572 <mr.biophilic383@gmail.com>
2026-08-23 00:23:55 +0530

    Complete asset CRUD APIs

--- Backend/routes/asset_routes.py
+++ Backend/routes/asset_routes.py
@@ -7,7 +7,7 @@ from models.asset_model import Asset
 asset_bp = Blueprint("asset", __name__)
 
 
-# Get all assets
+# GET all assets
 @asset_bp.route("/assets", methods=["GET"])
 def get_assets():
 
@@ -29,8 +29,29 @@ def get_assets():
     return jsonify(result)
 
 
+# GET single asset
+@asset_bp.route("/assets/<int:asset_id>", methods=["GET"])
+def get_asset(asset_id):
 
-# Add new asset
+    asset = Asset.query.get(asset_id)
+
+    if not asset:
+        return jsonify({
+            "message": "Asset not found"
+        }), 404
+
+    return jsonify({
+        "id": asset.id,
+        "asset_name": asset.asset_name,
+        "provider": asset.provider,
+        "service": asset.service,
+        "region": asset.region,
+        "status": asset.status,
+        "owner": asset.owner
+    })
+
+
+# POST - Add new asset
 @asset_bp.route("/assets", methods=["POST"])
 def add_asset():
 
@@ -50,4 +71,50 @@ def add_asset():
 
     return jsonify({
         "message": "Asset added successfully"
+    }), 200
+
+
+# PUT - Update asset
+@asset_bp.route("/assets/<int:asset_id>", methods=["PUT"])
+def update_asset(asset_id):
+
+    asset = Asset.query.get(asset_id)
+
+    if not asset:
+        return jsonify({
+            "message": "Asset not found"
+        }), 404
+
+    data = request.json
+
+    asset.asset_name = data.get("asset_name", asset.asset_name)
+    asset.provider = data.get("provider", asset.provider)
+    asset.service = data.get("service", asset.service)
+    asset.region = data.get("region", asset.region)
+    asset.status = data.get("status", asset.status)
+    asset.owner = data.get("owner", asset.owner)
+
+    db.session.commit()
+
+    return jsonify({
+        "message": "Asset updated successfully"
+    })
+
+
+# DELETE - Delete asset
+@asset_bp.route("/assets/<int:asset_id>", methods=["DELETE"])
+def delete_asset(asset_id):
+
+    asset = Asset.query.get(asset_id)
+
+    if not asset:
+        return jsonify({
+            "message": "Asset not found"
+        }), 404
+
+    db.session.delete(asset)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Asset deleted successfully"
     })
